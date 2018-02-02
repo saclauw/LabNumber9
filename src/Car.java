bcfac476439bb7c124927f97a7077e8367c4bfdf
saclauw <saclauw@gmail.com>
2018-02-01 22:08:13 -0500

    first commit of LabNumber9

---
+++ src/Car.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+
+/**
+ * @author saclauw
+ *
+ */
+public class Car {
+
+	public String make;
+	public String model;
+	public int year;
+	public double price;
+
+	public Car(String make, String model, int year, double price) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		this.price = price;
+
+		setMake(make);
+		setModel(model);
+		setYear(year);
+		setPrice(price);
+	}
+
+	public Car() {
+		make = "Ford";
+		model = "Flex";
+		year = 2011;
+		price = 17000.00;
+	}
+
+	public String getMake(String make) {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel(String model) {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getYear(int year) {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public double getPrice(double price) {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+
+		return String.format("%1$-15s %2$-10s %3$-6d $%4$-10.2f", make, model, year, price);
+	}
+	// return String.format("%1$-15s %2$-10s %3$-6d $%4$-10.2f \n", make, model,
+	// year, price);
+
+}
---
+++ src/CarApp.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CarApp {
+
+	public static void main(String[] args) {
+		int i = 0;
+		String make = "Ford";
+		String model = "Flex";
+		int year = 2011;
+		double price = 17000.00;
+
+		ArrayList<Car> carLot = new ArrayList<Car>();
+		Car car = new Car(make, model, year, price);
+		System.out.println("Welcome to the Grand Circus Motors admin console!");
+		Scanner scan = new Scanner(System.in);
+		// prompt the user for how many cars they want to enter
+		System.out.print("Please enter how many cars do you want to enter: ");
+		// get user input and store user input in variables
+		// each car take input of details: Make, model, year, price...(String, String,
+		// int, double)
+		// forLoop will iterate for as many cars as user inputs
+		int numOfCars = scan.nextInt();
+		for (i = 0; i < numOfCars; i++) {
+			System.out.print("What kind of make is the car?: ");
+			make = scan.next();
+			System.out.print("What kind of model is the car?: ");
+			model = scan.next();
+			System.out.print("What year is the car?: ");
+			year = scan.nextInt();
+			scan.nextLine();
+			System.out.print("What price is the car?: ");
+			price = scan.nextDouble();
+			car = new Car(make, model, year, price);
+			carLot.add(car);
+		}
+		System.out.println("...");
+		System.out.println("Current Inventory: ");
+		for (i = 0; i < carLot.size(); i++) {
+			System.out.println(carLot.get(i).toString());
+		}
+
+		// each iteration will prompt then get input for all details
+		// print out table of cars
+
+		scan.close();
+	}
+
+}
